2a51a32b92d7906041bb3f3d4b7d7b883abfe369
Andrey V. Semenushkin <andrey@semenushkin.ru>
2016-11-13 21:47:04 +0000

    Add changes in ask/ask/urls.py

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
+#sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+#sudo ln -sf /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
 #gunicorn -c /home/box/web/etc/hello.py hello:app
 #gunicorn -c /home/box/web/etc/ask.py ask:app
 sudo /etc/init.d/gunicorn restart
