55c25ecc28f53803fa0e3a0ce49664140297ddc8
Andrey V. Semenushkin <andrey@semenushkin.ru>
2016-11-13 23:38:12 +0300

    Add pythin-project ask

--- etc/hello.py
+++ etc/hello.py
@@ -1,11 +1,3 @@
-CONFIG = {
-	'mode': 'wsgi',
-	'working_dir': '/home/box/web',
-	'args': (
-		'--bind=0.0.0.0:8080',
-		'--daemon',
-		'--workers=2',
-		'--timeout=60',
-		'hello:app'
-	)
-}
+pythonpath = '/home/box/web'
+bind = '0.0.0.0:8080'
+workers = 4
