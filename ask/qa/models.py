d4726f946b7558da495a8cc50c7df4ce4ee1860d
Andrey V. Semenushkin <andrey@semensuhkin.ru>
2016-11-20 19:44:50 +0000

    first step in ORM

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+class Question(models.Model):
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	added_at = models.DateTimeField(auto_now=True,default=now())
+	rating = models.IntegerField(default=0)
+	author = models.ForeignKey(User, related_name='question_author', default=1)
+	likes = models.ManyToManyField(User, related_name='question_likes',default=0)
+
+	def get_absolute_url(self):
+		return reverse('question',kwargs={"id": self.id})
+	
+	def __unicode__(self):
+		return self.title
+
+class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DateTimeField(auto_now=True,default=now())
+	question = models.ForeignKey(Question)
+	author = models.ForeignKey(User, default=1,null=True)
--- init.sh
+++ init.sh
@@ -5,3 +5,4 @@ sudo /etc/init.d/nginx restart
 #gunicorn -c /home/box/web/etc/hello.py hello:app
 #gunicorn -c /home/box/web/etc/ask.py ask:app
 sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/mysql start
